6d89dfda8a764f7e3fab3a0eeda848057f97f9fe
dima <dimitrioslisenko@gmail.com>
2021-04-28 14:16:43 +0100

    Point root to posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :comments
-  resources :posts
+	root 'posts#index'
+	resources :comments
+	resources :posts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
